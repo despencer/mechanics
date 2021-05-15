6e2013c7692cd675f8a471a7621818c11cff73c2
despencer <despencer@mail.ru>
2021-05-15 18:50:29 +0300

    A little cleanup.

--- bracket.py
+++ bracket.py
@@ -45,27 +45,6 @@ doc.BracketBody.newObject('Sketcher::SketchObject','BracketSketch')
 doc.BracketSketch.Support = (doc.XZ_Plane, [''])
 doc.BracketSketch.MapMode = 'FlatFace'
 
-# doc.BracketSketch.addGeometry(Part.LineSegment(App.Vector(10,0,0),App.Vector(20,0,0)),False)
-# doc.BracketSketch.addGeometry(Part.LineSegment(App.Vector(20,0,0),App.Vector(30,0,0)),False)
-# doc.BracketSketch.addGeometry(Part.LineSegment(App.Vector(10,10,0),App.Vector(20,20,0)),False)
-# doc.BracketSketch.addGeometry(Part.LineSegment(App.Vector(20,10,0),App.Vector(30,20,0)),False)
-# doc.recompute()
-
-# doc.BracketSketch.addConstraint(Sketcher.Constraint('Coincident',0,1,-1,1))
-
-# doc.BracketSketch.addConstraint(Sketcher.Constraint('Coincident',0, 2, 1, 1))
-
-# doc.BracketSketch.addConstraint(Sketcher.Constraint('Horizontal', 0))
-# doc.recompute()
-
-# doc.BracketSketch.addConstraint(Sketcher.Constraint('Vertical', 1))
-
-# doc.BracketSketch.addConstraint(Sketcher.Constraint('DistanceX', 0, 1, App.Units.Quantity('0 mm')))
-# doc.BracketSketch.addConstraint(Sketcher.Constraint('DistanceY', 0, 1, App.Units.Quantity('0 mm')))
-
-# doc.BracketSketch.addConstraint(Sketcher.Constraint('DistanceX', 0, 1, 0, 2, App.Units.Quantity('30 mm')))
-# doc.BracketSketch.addConstraint(Sketcher.Constraint('DistanceY', 1, 1, 1, 2, App.Units.Quantity('15 mm')))
-
 makeU(doc.BracketSketch, width='30 mm', height='15 mm', thickness='4 mm')
 
 doc.recompute()
